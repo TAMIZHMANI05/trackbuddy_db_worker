42d775a859ccc1bda065481e29643187b937976d
Tamizhmani05 <tamizhmani13270@gmail.com>
2026-03-07 23:56:03 +0530

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import logger from './utils/logger';
+
+
+(() => {
+    try {
+        logger.info(`APPLICATION_STARTED`, {
+        });
+    } catch (err) {
+        logger.error(`APPLICATION_ERROR`, { meta: err });
+    }
+})();
